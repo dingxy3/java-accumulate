9f836118b0d81e18e7b3a37c4db6e6fd237eb289
dingxy3 <dingxy3@asiainfo.com>
2019-03-25 22:07:05 +0800

    1、静态代码块>非静态代码块》构造函数 2、非静态代码块每次在构造方法调用之前被执行

---
+++ src/main/java/day/ClassLoaderOrder.java
@@ -0,0 +1,35 @@
+package day;
+
+/**
+ * @param
+ * @Author: dingxy3
+ * @Description:类初始化加载顺序
+ * 非静态代码块在类每次没使用或者实例化的时候被执行
+ * 静态代码块只在第一次时执行一次
+ * @Date: Created in  2019/3/25
+ **/
+public class ClassLoaderOrder {
+
+    public static void main(String args[]){
+        run();
+    }
+
+    static ClassLoaderOrder demo = new ClassLoaderOrder();
+    static
+    {
+        System.out.println("1");
+    }
+    {
+        System.out.println("2");
+    }
+    public ClassLoaderOrder(){
+        System.out.println("3");
+        System.out.println("a="+a+",b="+b);
+    }
+    public static void run(){
+        System.out.println("4");
+    }
+    int a = 100;
+    static int b = 200;
+   // static ClassLoaderOrder demo = new ClassLoaderOrder();
+}
